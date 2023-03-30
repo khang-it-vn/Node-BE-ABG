612e86b7bf2ab4d5699080bdc0df862e5ceac71c
Tung@k19 <tranminhtung18112001@gmail.com>
2023-03-30 15:50:54 +0700

    Add source for admin service

---
+++ app/models/admin.js
@@ -0,0 +1,24 @@
+const Sequelize = require('sequelize');
+const sequelize = require('../database/database');
+
+const Admin = sequelize.define('admin', {
+  id_admin: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  fullname: {
+    type: Sequelize.STRING,
+    allowNull: true
+  },
+  type: {
+    type: Sequelize.INTEGER,
+    allowNull: true
+  }
+},{ tableName: 'admin' ,timestamps: false});
+
+module.exports = Admin;
